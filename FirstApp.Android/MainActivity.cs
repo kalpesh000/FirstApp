7cbfc88c137d834c6a513cbbeb50c17564504141
Kalpesh <kalpeshmhase000@gmail.com>
2019-09-10 08:27:13 +0530

    Added Firestore sercice and pushed a string into firestore database.

--- FirstApp.Android/MainActivity.cs
+++ FirstApp.Android/MainActivity.cs
@@ -17,7 +17,7 @@ namespace FirstApp.Droid
     [Activity(Label = "FirstApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        //Firestore Global initialization. 
+        //Firestore Global initialization.
         FirebaseFirestore database;
         protected override void OnCreate(Bundle savedInstanceState)
         {
